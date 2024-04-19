475b3fe1f63b1598df8dd21f07218e4cb5ec2ba4
Paul Medynski <paul.medynski@nuance.com>
2024-04-19 12:15:59 -0300

    Created a sample project to test different POM settings related to --add-exports.

---
+++ src/main/java/ca/medynski/test/Main.java
@@ -0,0 +1,12 @@
+package ca.medynski.test;
+
+import com.sun.org.apache.xerces.internal.util.SymbolTable;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        final SymbolTable table = new SymbolTable();
+        table.addSymbol("Hello");
+    }
+}
